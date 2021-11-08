2d56cd2899744daeaa0410887e7058db653ffd68
alan <al_coimbra@hotmail.com>
2021-11-08 18:47:17 -0300

    First Commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://alcoimbra:!01111988@cursojs01.q5j9w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
